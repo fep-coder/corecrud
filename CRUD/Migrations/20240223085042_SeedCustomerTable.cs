60f4ad292560b912e21eeb139abbb768bd20d786
voja <voja@gmail.com>
2024-02-23 09:51:55 +0100

    7 Seed Customer table

---
+++ CRUD/Migrations/20240223085042_SeedCustomerTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CRUD.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCustomerTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "Id", "Email", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, "john@gmail.com", "John", "Jones" },
+                    { 2, "mary@gmail.com", "Mary", "Jones" },
+                    { 3, "jack@gmail.com", "Jack", "Smith" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
--- CRUD/Models/ApplicationDbContext.cs
+++ CRUD/Models/ApplicationDbContext.cs
@@ -9,5 +9,14 @@ namespace CRUD.Models
         }
 
         public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().HasData(
+                new Customer { Id = 1, FirstName = "John", LastName = "Jones", Email = "john@gmail.com" },
+                new Customer { Id = 2, FirstName = "Mary", LastName = "Jones", Email = "mary@gmail.com" },
+                new Customer { Id = 3, FirstName = "Jack", LastName = "Smith", Email = "jack@gmail.com" }
+            );
+        }
     }
 }
